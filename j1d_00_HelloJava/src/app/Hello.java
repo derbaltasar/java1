a484a1c9d3252e2bb1d8603e19b4741a878d6283
Student <Student@10.101.102.104>
2015-06-03 15:33:02 +0200

    Hallo Welt

---
+++ j1d_00_HelloJava/src/app/Hello.java
@@ -0,0 +1,37 @@
+package app;
+
+
+/**
+ * Kommentar für Javadoc
+ * 
+ * @author Student
+ *
+ */
+public class Hello {
+	
+	
+	/**
+	 * Methoden-Definition
+	 */
+	public  void methode1(){
+		System.out.println("methode1...");
+		
+	}
+	
+		
+	/*
+	 * mehrzeiliger Kommetar
+	 */
+	public static void main(String[] args) {
+		// Programm start
+		
+		System.out.println("Hallo Java"); // einzeiliger Kommentar
+		
+	
+		
+		Hello  h = new Hello();
+		h.methode1();
+		// Programm ende
+	}
+
+}

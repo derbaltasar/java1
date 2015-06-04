a123640f55ef6bd32c5ec900695e5e158df32604
Student <Student@10.101.102.104>
2015-06-04 15:01:08 +0200

    Bilder ienbinden

---
+++ j1d_19_fx_Images/src/application/SampleController.java
@@ -0,0 +1,20 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class SampleController {
+	
+	
+	@FXML
+	ImageView imgView;
+	
+	public void setImage(ActionEvent e){
+		imgView.setImage(new Image("img/Penguins.jpg"));
+	}
+	
+	
+	
+}
